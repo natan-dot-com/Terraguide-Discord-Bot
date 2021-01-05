86ab865e393950e38eb1ab3c26ace53c7f8b5d26
AndreiAlisson <andreialesson@hotmail.com>
2021-01-05 14:51:25 -0300

    Initial bot test

--- main_bot.py
+++ main_bot.py
@@ -7,11 +7,11 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
-async def on_message():
+async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startwith('$hello'):
+    if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('Nzk2MDYzMTE0NDk1NTI0OTI1.X_SdjA.dZnQYOHJJqmjxoNeIp2lCnXQID8')
+client.run('MjQ2NTExOTcxMDY5ODUzNjk3.WCVcKQ.quxR1uO0TUb6UQPhvLYzqoApHBI')
