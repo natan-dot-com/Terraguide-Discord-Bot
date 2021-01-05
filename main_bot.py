5d9017bfae8e20f89172b5a632e361273c6b291a
Natan Sanches <natanhsanches@gmail.com>
2021-01-05 14:46:35 -0300

    Fixed bugs and readme

--- main_bot.py
+++ main_bot.py
@@ -7,11 +7,11 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
-async def on_message():
+async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startwith('$hello'):
+    if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('Nzk2MDYzMTE0NDk1NTI0OTI1.X_SdjA.dZnQYOHJJqmjxoNeIp2lCnXQID8')
+client.run('Nzk2MDY1OTI0NzU1MDk1NTg0.X_SgKg.8UNAsVGPDnbS2nMc40LrpuoepTI')
