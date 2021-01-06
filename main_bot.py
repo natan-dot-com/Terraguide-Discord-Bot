76d7a41b8323ffe62c8a441f68ede9456a44a26d
Natan <natanhsanches@gmail.com>
2021-01-06 11:50:31 -0300

    Begin bot's main file

--- main_bot.py
+++ main_bot.py
@@ -1,4 +1,7 @@
 import discord
+from discord.ext import commands
+import json_manager
+import utility
 
 client = discord.Client()
 
@@ -11,7 +14,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$ping'):
+        await message.channel.send('Pong!')
+
+@client.command()
+async def recp(ctx, item):
+    if ctx.author == client.user or not item:
+        return
+
+    returnMessage = ""
+
 
 client.run('Nzk2MDY1OTI0NzU1MDk1NTg0.X_SgKg.8UNAsVGPDnbS2nMc40LrpuoepTI')
