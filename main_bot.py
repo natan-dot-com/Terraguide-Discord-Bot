192a7986ea3bff8c40ea5fdf66cea82f66e678c4
AndreiAlisson <andreialesson@hotmail.com>
2021-01-07 00:50:04 -0300

    Cleaner code and more details

--- main_bot.py
+++ main_bot.py
@@ -1,17 +1,21 @@
 import discord
 from discord.ext import commands
 import json
-from search import binary_search
+from tools import *
 
-recipeNameList = ['recipe1', 'recipe2', 'recipe3', 'recipe4', 'recipe5', 'recipe6']
-ingredientNameList = ['ingredient1', 'ingredient2', 'ingredient3', 'ingredient4', 'ingredient5', 'ingredient6']
-amountNameList = ['amount1', 'amount2', 'amount3', 'amount4', 'amount5', 'amount6']
-
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='$', description=description, help_command=None)
 
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
+
+@bot.command()
+async def help(ctx):
+    
+    if ctx.author == bot.user:
+        return
+    
+    await ctx.send('```Command List:\n$fr "ItemName" -> Search for the recipe of an item```')
 
 @bot.command()
 async def fr(ctx, arg):
@@ -24,61 +28,58 @@ async def fr(ctx, arg):
     with open('json/items.json') as items:
         itemList = json.load(items)        
 
-        for itemInstance in itemList:
-            
-            if itemInstance['name'] == arg:
-                with open('json/recipes.json') as recipes:
-                    recipeList = json.load(recipes)
+        #Search for the given item name
+        itemInstance = search_by_name(itemList, arg)
+        if not itemInstance:
+            await ctx.send('Item not found. Be sure to spell the item name correctly in quotes')
+            return NOT_FOUND
+        
+        with open('json/recipes.json') as recipes:
+            recipeList = json.load(recipes)
 
-                for recipeName in recipeNameList:
-                    if itemInstance[recipeName]:
-                        recipeInstance = binary_search(recipeList, 0, len(recipeList), int(itemInstance[recipeName]))
+            #Check each of the recipes
+            for recipeName in recipeNameList:
+
+                #if the JSON doesn't have any recipes left then break
+                if not itemInstance[recipeName]:
+                    if not message:
+                        await ctx.send("This item doesn't have any recipe")
+                    break
+
+                #reset the output variable for the next possible recipe
+                message = ""
+
+                recipeInstance = search_by_id(recipeList, 0, len(recipeList), int(itemInstance[recipeName]))
+        
+                if not recipeInstance:
+                    return ERROR  
+
+                with open('json/tables.json') as tables:
+                    tableList = json.load(tables)
+                    tableInstance = search_by_id(tableList, 0, len(tableList), int(recipeInstance['table']))
+
+                message = message + ":hammer_pick: Item " + itemInstance['name'] + " is made on :hammer_pick:\n" + tableInstance['name'] + "\n"
                 
-                        if recipeInstance:
-                            message = message + ":hammer_pick: Item " + itemInstance['name'] + " is made by using :hammer_pick:\n"
-                        else:
-                            break   
+                if tableInstance['alternate_name']:
+                    message = message + tableInstance['alternate_name'] + "\n"
 
-                        for ingredientName, amountName in zip(ingredientNameList, amountNameList):
-                            if recipeInstance[ingredientName]:
-                                ingredientInstance = binary_search(itemList, 0, len(itemList), int(recipeInstance[ingredientName]))
-                                if ingredientInstance:
-                                    message = message + recipeInstance[amountName] + " " + ingredientInstance['name'] + "\n"
-                                else:
-                                    break
-                        
-                        await ctx.send(message)
+                message = message + ":gear: and uses the following ingredients :gear:\n"             
+
+                #Search for each of the ingredients
+                for ingredientName, amountName in zip(ingredientNameList, amountNameList):
+
+                    #if the JSON doesn't have any ingredients left then break
+                    if not recipeInstance[ingredientName]:
+                        break
+
+                    ingredientInstance = search_by_id(itemList, 0, len(itemList), int(recipeInstance[ingredientName]))
                     
-                    else:
-                        break
+                    if not ingredientInstance:
+                        return ERROR
 
-                    message = ""
-                return
-        
-        await ctx.send('Recipe not found')
-        
-        '''for recipeInstance in recipeList:
-                    #Search for recipe
-                    if recipeInstance['id'] == itemInstance['recipe1']:
-                        message = message + ":hammer_pick: Item " + itemInstance['name'] + " is made by using :hammer_pick:\n"
-                        if len(recipeInstance['ingredient1']) > 0:
-
-                            for itemInstance in itemList:
-                                #Search for ingredient 1
-                                if itemInstance['id'] == recipeInstance['ingredient1']:
-                                    message = message + recipeInstance['amount1'] + " " + itemInstance['name'] + "\n"
-                                    break
-                        if len(recipeInstance['ingredient2']) > 0:
-
-                            for itemInstance in itemList:
-                                #Search for ingredient 2
-                                if itemInstance['id'] == recipeInstance['ingredient2']:
-                                    message = message + recipeInstance['amount2'] + " " + itemInstance['name'] + "\n"
-                                    break
-                        break
-                await ctx.send(message)
-                return
-                break
-        await ctx.send('Recipe not found')'''
+                    message = message + recipeInstance[amountName] + " " + ingredientInstance['name'] + "\n"
+                
+                #Send the message to UI discord after all the ingredients were put into the ouput message variable
+                await ctx.send(message)               
 
 bot.run('MjQ2NTExOTcxMDY5ODUzNjk3.WCVcKQ.quxR1uO0TUb6UQPhvLYzqoApHBI')
