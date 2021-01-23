ae9e5a7370b7b595f5e4a5cbef336b5603fac974
AndreiAlisson <andreialesson@hotmail.com>
2021-01-23 17:25:17 -0300

    Small Fixes on crafting scraping

--- data_scraping/items/crafting_scraping.py
+++ data_scraping/items/crafting_scraping.py
@@ -9,59 +9,76 @@ import requests
 import json
 import re
 
-URL = "https://terraria.gamepedia.com/Iron_Hammer"
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-counter = 1
+itemList = LoadJSONFile('json_new/items.json')
+CRAFTINGS_PATH = 'json_new/craftings.json'
+craftsList = []
+craftId = 1
 
-craftingTable = soup.find("div", class_="crafts")
-if craftingTable:
-    rows = craftingTable.findAll("tr")
-    for row in rows[1::]:
-        mainDict = {
-            "craft_id": "",
-            "craft_qty": "",
-            "table": [],
-            "recipe": []
-        }
-        mainDict["craft_id"] = str(counter)
+for item in itemList:
+
+    '''if craftId > 50:
+        break'''
+
+    print("finding " + item['name'])
+    URL = "https://terraria.gamepedia.com/" + item['name'].replace(" ", "_")
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    craftingTable = soup.find("div", class_="crafts")
+    if craftingTable and re.search("Recipes*", str(soup.find("h3").find("span", class_="mw-headline"))):
         
-        # Finding which item it's being crafted
-        if row.find("td", class_="result"):
-            result_code = row.find("td", class_="result")
-            result = result_code.img['alt']
-            qty = result_code.find("span", class_="note-text")
-            if not qty:
-                mainDict["craft_qty"] = '1'
-            else:
-                mainDict["craft_qty"] = ' '.join(re.findall("\((\d+)\)", qty.text))
-        
-        # Finding all ingredients with its respective quantities relative to the recipe
-        if row.find("td", class_="ingredients"):
-            ingredientsList = row.find("td", class_="ingredients").findAll("li")
-            for ingredientInstance in ingredientsList:
-                ingredientsDict = {
-                    "ingredient": "",
-                    "qty": ""
-                }
-                ingredientsDict["ingredient"] = ingredientInstance.img['alt']
-                qty = ingredientInstance.find("span", class_="note-text")
-                if qty:
-                    ingredientsDict["qty"] = ' '.join(re.findall("\((\d+)\)", qty.text))
+        rows = craftingTable.findAll("tr")
+        for row in rows[1::]:
+            craftDict = {
+                "craft_id": "",
+                "craft_qty": "",
+                "table": [],
+                "recipe": []
+            }
+            craftDict["craft_id"] = str(craftId)
+            
+            # Finding which item it's being crafted
+            if row.find("td", class_="result"):
+                result_code = row.find("td", class_="result")
+                result = result_code.img['alt']
+                qty = result_code.find("span", class_="note-text")
+                if not qty:
+                    craftDict["craft_qty"] = '1'
                 else:
-                    ingredientsDict["qty"] = '1'
-                mainDict["recipe"].append(ingredientsDict)
-                
-        # Finding all the crating stations in which the recipe can be made
-        if row.find("td", class_="station"):
-            stationList = row.find("td", class_="station").findAll("span", class_="i")
-            if not stationList:
-                stationList = ["By Hand"]
+                    craftDict["craft_qty"] = ' '.join(re.findall("\((\d+)\)", qty.text))
+            else:
+                craftDict["craft_qty"] = craftsList[-1]["craft_qty"]
+            
+            # Finding all ingredients with its respective quantities relative to the recipe
+            if row.find("td", class_="ingredients"):
+                ingredientsList = row.find("td", class_="ingredients").findAll("li")
+                for ingredientInstance in ingredientsList:
+                    ingredientsDict = {
+                        "ingredient": "",
+                        "qty": ""
+                    }
+                    ingredientsDict["ingredient"] = ingredientInstance.img['alt']
+                    qty = ingredientInstance.find("span", class_="note-text")
+                    if qty:
+                        ingredientsDict["qty"] = ' '.join(re.findall("\((\d+)\)", qty.text))
+                    else:
+                        ingredientsDict["qty"] = '1'
+                    craftDict["recipe"].append(ingredientsDict)
+                    
+            # Finding all the crating stations in which the recipe can be made
+            if row.find("td", class_="station"):
+                stationList = row.find("td", class_="station").findAll("span", class_="i")
+                if not stationList:
+                    stationList = ["By Hand"]
             for stationInstance in stationList:
                 if not stationInstance == "By Hand":
-                    mainDict["table"].append(stationInstance.span.span.a.text)
+                    craftDict["table"].append(stationInstance.span.span.a.text)
                 else:
-                    mainDict["table"].append(stationInstance)
-            
-        print(json.dumps(mainDict, indent=4))
-        counter += 1
+                    craftDict["table"].append(stationInstance)
+
+            craftsList.append(craftDict)
+            craftId += 1
+
+    else:
+        print(item["name"] + " recipe not found")
+    
+SaveJSONFile(CRAFTINGS_PATH, craftsList)
--- hash_test/hash_test.py
+++ hash_test/hash_test.py
@@ -1,11 +1,16 @@
 from item_hash import *
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir) 
 from json_manager import *
+
 
 ITEMS_DICT_STRING_INDEX = 'name'
 ITEMS_DICT_INDEX_TO_RETURN = 'id'
 ITEMS_TABLE_SIZE = 8192
 
-itemList = LoadJSONFile('items.json')
+itemList = LoadJSONFile('json_new/items.json')
 hashMap = hashTable(ITEMS_TABLE_SIZE, ITEMS_DICT_STRING_INDEX)
 for itemInstance in itemList:
     hashMap.hashString(itemInstance[ITEMS_DICT_STRING_INDEX], itemInstance)
