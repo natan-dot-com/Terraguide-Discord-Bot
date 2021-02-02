767247edcdcec2974cef8ee8f415bbbdb408700d
AndreiAlisson <andreialesson@hotmail.com>
2021-02-02 13:40:56 -0300

    Adjusted backgrounds scraping

--- data_scraping/items/backgrounds_scraping.py
+++ data_scraping/items/backgrounds_scraping.py
@@ -5,7 +5,6 @@ parent_dir = os.path.dirname(parent_dir)
 sys.path.insert(0, parent_dir)
 from scraping_tools import *
 from json_manager import *
-import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -19,7 +18,6 @@ DUNGEON_WALLS = {
     "Blue Slab Wall", "Green Slab Wall", "Pink Slab Wall",
     "Blue Tiled Wall", "Green Tiled Wall", "Pink Tiled Wall"
 }
-EXCEPTIONS = {"Crimson Heart", "Shadow Orb"}
 itemList = LoadJSONFile(ITEM_FILE_PATH)
 url = "https://terraria.gamepedia.com/"
 wallsList = []
@@ -31,7 +29,7 @@ for itemInstance in itemList:
         page = requests.get(newURL)
         soup = BeautifulSoup(page.content, "html.parser")
 
-        if not itemInstance[SCRAPING_NAME] in EXCEPTIONS and not itemInstance[SCRAPING_NAME] in DUNGEON_WALLS:
+        if not itemInstance[SCRAPING_NAME] in DUNGEON_WALLS:
             tableBoxes = soup.find_all("div", class_="infobox item")
         elif itemInstance[SCRAPING_NAME] in DUNGEON_WALLS:       
             tableBoxes = soup.find_all("div", class_="infobox item float-left")
